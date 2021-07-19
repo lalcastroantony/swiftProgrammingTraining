9347862aeb849d95ee1f52acd0ad4c180c844843
Lal Castro <lalcastro.a@zohocorp.com>
2021-07-19 12:46:04 +0530

    2.Constansts and Variables

---
+++ SwiftTraining.playground/Contents.swift
@@ -0,0 +1,13 @@
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+let x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+//=============================================================
+
+
