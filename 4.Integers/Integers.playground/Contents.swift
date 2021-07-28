80fe6990dc02a16ed1972bed243565a71c16f708
Lal Castro <lalcastro.a@zohocorp.com>
2021-07-28 06:54:45 +0530

    Integers

---
+++ 4.Integers/Integers.playground/Contents.swift
@@ -0,0 +1,36 @@
+//Integers
+let number = 11
+//Types
+//Signed Integers
+
+let signedNumber: Int = -8
+//uncomment the below line to see the error
+//let someNumber: Int = 5.5
+
+//UnsignedIntegers
+let unsignedNumber: UInt = 898
+//Uncomment the below line to see the error
+//let someUnSignedNumber: UInt = -10
+
+//Min max of signed:
+print(Int8.min)
+print(Int8.max)
+print(Int16.min)
+print(Int16.max)
+print(Int32.min)
+print(Int32.max)
+print(Int64.min)
+print(Int64.max)
+print(Int.min)
+print(Int.max)
+//Min max of Unsigned:
+print(UInt8.min)
+print(UInt8.max)
+print(UInt16.min)
+print(UInt16.max)
+print(UInt32.min)
+print(UInt32.max)
+print(UInt64.min)
+print(UInt64.max)
+print(UInt.min)
+print(UInt.max)
---
+++ 4.Integers/README.md
@@ -0,0 +1,10 @@
+# 4.Integers
+
+* Whole numbers with no fractional component
+* Types: Signed(positive, zero, negative), Unsigned(zero,positive)
+* Integer Bounds (min,max)
+* Int type (same size as current platform’s native word size) 
+* On 32 bit, Int is same as Int32, On 64 bit Int is same as Int64
+* UInt - UInt is same as UInt32, On 64 bit Int is same as Unt64
+
+https://www.youtube.com/watch?v=Pomorz9U3sg
