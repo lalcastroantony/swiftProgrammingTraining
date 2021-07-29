275eb85bb5b13e71435d1fd7d72c2e7c8bd87e59
Lal Castro <lalcastro.a@zohocorp.com>
2021-07-29 09:47:49 +0530

    Float,Type safety,Type Inference

---
+++ 5.Float, Type Safety, Type Inference/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//5.Float, Type Safety, Type Inference
+//
+//* Numbers with fractional components
+//* Double (64-bit Floating point numbers)
+//* Float (32-bit Floating point numbers)
+//* Swift - Type safe language (Ex: Can’t pass integer where the string is required)
+//* Type Inference - with an initial value/output of expression
+
+
+var aNumber:Double = 7.8
+aNumber = 6.9
+//Uncomment the following to see the error
+//aNumber = "Sksdjfsakj"
+
+let pi = 3.14159
+
+let anotherPi = 3 + 0.14159
+
+//Uncomment the following to see the error
+//let someInt: Int = 1 + 8.994
---
+++ 5.Float, Type Safety, Type Inference/README.md
@@ -0,0 +1,8 @@
+# 5.Float, Type Safety, Type Inference
+* Numbers with fractional components
+* Double (64-bit Floating point numbers)
+* Float (32-bit Floating point numbers)
+* Swift - Type safe language (Ex: Can’t pass integer where the string is required)
+* Type Inference - with an initial value/output of expression
+
+https://www.youtube.com/watch?v=Ikm6iehf-Pg
