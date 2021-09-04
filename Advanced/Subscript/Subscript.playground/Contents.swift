e9ed133454695971d7173ea54beac9221c60e3ec
Lal Castro <lalcastro.a@zohocorp.com>
2021-09-04 11:52:01 +0530

    Subscripts

---
+++ Advanced/Subscript/Subscript.playground/Contents.swift
@@ -0,0 +1,74 @@
+//Subscripts
+
+//subscript(index: Int) -> Int {
+//    get {
+//        // Return an appropriate subscript value here.
+//    }
+//    set(newValue) {
+//        // Perform a suitable setting action here.
+//    }
+//}
+
+//As with read-only computed properties, you can simplify the declaration of a read-only subscript by removing the get keyword and its braces:
+
+//subscript(index: Int) -> Int {
+//    // Return an appropriate subscript value here.
+//}
+
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
+// Prints "six times three is 18"
+
+
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+
+//unlike functions, subscripts can’t use in-out parameters.
+
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
+
+
+
+//Type Subscripts
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+let mars = Planet[4]
+print(mars)
