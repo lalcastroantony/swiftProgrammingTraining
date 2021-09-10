50176e267426042c762998ed3cbcd94e48325706
Lal Castro <lalcastro.a@zohocorp.com>
2021-09-10 19:04:32 +0530

    InitializersDeinitialization

---
+++ Advanced/Deinitialization/Deinitialization.playground/Contents.swift
@@ -0,0 +1,52 @@
+class someClass {
+    deinit {
+        // perform the deinitialization
+    }
+}
+
+
+//Superclass deinitializers are always called, even if a subclass doesn’t provide its own deinitializer.
+
+
+class Bank {
+    static var coinsInBank = 10_000
+    static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+        let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
+        coinsInBank -= numberOfCoinsToVend
+        return numberOfCoinsToVend
+    }
+    static func receive(coins: Int) {
+        coinsInBank += coins
+    }
+}
+
+class Player {
+    var coinsInPurse: Int
+    init(coins: Int) {
+        coinsInPurse = Bank.distribute(coins: coins)
+    }
+    func win(coins: Int) {
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    deinit {
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+var playerOne: Player? = Player(coins: 100)
+print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
+// Prints "A new player has joined the game with 100 coins"
+print("There are now \(Bank.coinsInBank) coins left in the bank")
+// Prints "There are now 9900 coins left in the bank"
+
+playerOne!.win(coins: 2_000)
+print("PlayerOne won 2000 coins & now has \(playerOne!.coinsInPurse) coins")
+// Prints "PlayerOne won 2000 coins & now has 2100 coins"
+print("The bank now only has \(Bank.coinsInBank) coins left")
+// Prints "The bank now only has 7900 coins left"
+
+playerOne = nil
+print("PlayerOne has left the game")
+// Prints "PlayerOne has left the game"
+print("The bank now has \(Bank.coinsInBank) coins")
+// Prints "The bank now has 10000 coins"
