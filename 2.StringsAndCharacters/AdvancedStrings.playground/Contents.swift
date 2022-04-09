b29508343dbd6e4ff4927360dc1b88182ccb498a
lalcastro.a <lalcastro.a@zohocorp.com>
2022-04-09 12:56:10 +0530

    till collections

--- 2.StringsAndCharacters/AdvancedStrings.playground/Contents.swift
+++ 2.StringsAndCharacters/AdvancedStrings.playground/Contents.swift
@@ -1,202 +1,534 @@
+//Strings and Characters
 
 
+//What is a String?
 
 
+//String Literals
 
+//print("Lal Castro")
 
+//Multiline String Literals
 
+let aMultilineString = """
+fjljglfsjkdg
 
+fdg
+sg
+d
+dsf
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Strings Advanced Level
-
-//Mutliline with " inside without using escaping characters
-let multiLine = """
-"hello"
 """
-print(multiLine)
+
+//print(aMultilineString)
 
 
-let exclamationMark: Character = "!"
-let catCharacters: [Character] = ["C", "a", "t", "!", "🐱"]
+//Special Characters in String Literals with \
 
-var welcome = "Welcome"
+//print("\"hello\"")
 
-welcome.append(exclamationMark)
+//Extended String Delimiters with #
 
-//You can use extended string delimiters to create strings containing characters that would otherwise be treated as a string interpolation. For example:
-print(#"Write an interpolated string in Swift using \(multiplier)."#)
-// Prints "Write an interpolated string in Swift using \(multiplier)."
-
+//
+//print("Line 1\nLine 2")
+//print(#"Line 1\nLine 2"#)
 
 
-//To use string interpolation inside a string that uses extended delimiters, match the number of number signs after the backslash to the number of number signs at the beginning and end of the string. For example:
+//Initializing an Empty String
 
-print(#"6 times 7 is \#(6 * 7)."#)
-// Prints "6 times 7 is 42."
+let word = ""
+let anotherEmptyWord = String()
+
+
+
+
+//checking if a string is empty or not
+if word.isEmpty {
+//    print("it is empty")
+}
+
+//String Mutability with +=
+
+var aString = "hello"
+aString += "world"
+
+//print(aString)
+
+//Strings Are Value Types
+
+var a = "hi"
+var b = a
+
+b = "hello"
+
+//print(a)
+//print(b)
+
+//Working with Characters
+
+let name = "Castro"
+
+for c in name {
+//    print(c)
+}
+
+
+//Concatenating Strings and Characters - adding two strings
+
+let one = "hello"
+let two = "world"
+
+let addedString = one + two
+
+//print(addedString)
+
+//String Interpolation
+
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+
+//print(message)
+
+
+
+
+
+
+
+
+
+//Unicode
+//Unicode is an international standard for encoding, representing, and processing text in different writing systems.
+
+//Unicode Scalar Values
+//Each character is formed by compined unicode scalars
 
 let eAcute: Character = "\u{E9}"                         // é
-let combinedEAcute: Character = "\u{65}\u{301}"          // e followed by
-// eAcute is é, combinedEAcute is é
+let combinedEAcute: Character = "\u{65}\u{301}"  // e followed by
 
-
-
-//Extended grapheme clusters enable scalars for enclosing marks (such as COMBINING ENCLOSING CIRCLE, or U+20DD) to enclose other Unicode scalars as part of a single Character value:
-
-
-let copyRight: Character = "\u{63}\u{20DD}"
-print(copyRight)
-//prints c⃝
-
-//Unicode scalars for regional indicator symbols can be combined in pairs to make a single Character value, such as this combination of REGIONAL INDICATOR SYMBOL LETTER U (U+1F1FA) and REGIONAL INDICATOR SYMBOL LETTER S (U+1F1F8):
-//https://en.wikipedia.org/wiki/Regional_indicator_symbol
+let enclosedEAcute: Character = "\u{E9}\u{20DD}"
+// enclosedEAcute is é⃝
 
 let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
 // regionalIndicatorForUS is 🇺🇸
 
-let regionalIndicatorForIndia: Character = "\u{1F1EE}\u{1F1F3}"
-// regionalIndicatorForIndia is 🇮🇳
 
-let stringVal:String = "\u{1F1EE}\u{1F1F3}"
-print(stringVal.count)
 
-let greeting = "Guten Tag!"
-print(greeting[greeting.index(before: greeting.endIndex)])
 
-//Uncomment to see the error
-//print(greeting[greeting.endIndex])
 
-print(greeting.startIndex) //startIndex will be 1, Not 0
 
-//To access an index farther away from the given index, you can use the index(_:offsetBy:)
-let index = greeting.index(greeting.startIndex, offsetBy: 7)
-print(greeting[index])
+
+
+
+
+
+
+
+
+
+
+
+//Counting Characters
+
+var wordd = "cafe"
+print("the number of characters in \(word) is \(word.count)")
+// Prints "the number of characters in cafe is 4"
+
+wordd += "\u{301}"    // COMBINING ACUTE ACCENT, U+0301
+
+print("the number of characters in \(wordd) is \(wordd.count)")
+// Prints "the number of characters in café is 4"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Accessing and Modifying a String
+
+//String Indices
+//Use the startIndex property to access the position of the first Character of a String. The endIndex property is the position after the last character in a String. As a result, the endIndex property isn’t a valid argument to a string’s subscript. If a String is empty, startIndex and endIndex are equal.
+
+//let greetingg = "Guten Tag!"
+//print(greetingg.startIndex)
+//print(greetingg.endIndex)
+//print(greetingg[greetingg.startIndex])
+//// G
+//print(greetingg[greetingg.index(before: greetingg.endIndex)])
+//// !
+//print(greetingg[greetingg.index(after: greetingg.startIndex)])
+//// u
+//let indexx = greetingg.index(greetingg.startIndex, offsetBy: 7)
+//print(greetingg[indexx])
 // a
 
-//Use the indices property to access all of the indices of individual characters in a string.
+//print(greetingg[0])
 
-for index in greeting.indices {
-    print("\(greeting[index]) ", terminator: "")
-}
-// Prints "G u t e n   T a g ! "
-
+//for index in greetingg.indices {
+////    print("\(greetingg[index]) ", terminator: "")
+//}
 
 
-var welcomeMessage = "hello"
-welcomeMessage.insert("!", at: welcomeMessage.endIndex)
-// welcome now equals "hello!"
-
-welcomeMessage.insert(contentsOf: " there", at: welcomeMessage.index(before: welcomeMessage.endIndex))
-// welcome now equals "hello there!"
-
-welcomeMessage.remove(at: welcomeMessage.index(before: welcomeMessage.endIndex))
-// welcome now equals "hello there"
-
-let range = welcomeMessage.index(welcomeMessage.endIndex, offsetBy: -6)..<welcomeMessage.endIndex
-welcomeMessage.removeSubrange(range)
-// welcome now equals "hello"
 
 
-let greetingMessage = "Hello, world!"
-let indexx = greetingMessage.firstIndex(of: ",") ?? greetingMessage.endIndex
-let beginning = greetingMessage[..<indexx]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Inserting and Removing
+
+var welcomee = "hello"
+welcomee.insert("!", at: welcomee.endIndex)
+// welcomee now equals "hello!"
+
+welcomee.insert(contentsOf: " there", at: welcomee.index(before: welcomee.endIndex))
+// welcomee now equals "hello there!"
+
+
+welcomee.remove(at: welcomee.index(before: welcomee.endIndex))
+// welcomee now equals "hello there"
+
+let rangee = welcomee.index(welcomee.endIndex, offsetBy: -6)..<welcomee.endIndex
+welcomee.removeSubrange(rangee)
+// welcomee now equals "hello"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Substrings
+
+let greetinggg = "Hello, world!"
+let indexxxx = greetinggg.firstIndex(of: ",") ?? greetinggg.endIndex
+let beginninggg = greetinggg[..<indexxxx]
 // beginning is "Hello"
 
 // Convert the result to a String for long-term storage.
-let newString = String(beginning)
-//Like strings, each substring has a region of memory where the characters that make up the substring are stored. The difference between strings and substrings is that, as a performance optimization, a substring can reuse part of the memory that’s used to store the original string, or part of the memory that’s used to store another substring.
+let newStringgg = String(beginninggg)
 
 
 
-// "Voulez-vous un café?" using LATIN SMALL LETTER E WITH ACUTE
-let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
 
-// "Voulez-vous un café?" using LATIN SMALL LETTER E and COMBINING ACUTE ACCENT
-let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
 
-if eAcuteQuestion == combinedEAcuteQuestion {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Comparing Strings
+
+//String and Character Equality
+let quotation = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
     print("These two strings are considered equal")
 }
 // Prints "These two strings are considered equal"
 
-let dogString = "Dog‼🐶" // the one used here is not the normal exclamation mark <!> it is <‼>
-for codeUnit in dogString.utf8 {
-    print("\(codeUnit) ", terminator: "")
-}
-print("")
-// Prints "68 111 103 226 128 188 240 159 144 182 "
-
-for codeUnit in dogString.utf16 {
-    print("\(codeUnit) ", terminator: "")
-}
-print("")
-// Prints "68 111 103 8252 55357 56374 "
 
 
-for scalar in dogString.unicodeScalars {
-    print("\(scalar.value) ", terminator: "")
-}
-print("")
-// Prints "68 111 103 8252 128054 "
+//Prefix and Suffix Equality
 
-for scalar in dogString.unicodeScalars {
-    print("\(scalar) ")
-}
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
 
-for scalar in dogString.unicodeScalars {
-    if scalar.properties.isEmoji {
-        print("\(scalar) is an emoji")
-    }
-    else {
-        print("\(scalar) is not an emoji")
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+        act1SceneCount += 1
     }
 }
+print("There are \(act1SceneCount) scenes in Act 1")
+// Prints "There are 5 scenes in Act 1"
 
 
-print(dogString.count)
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        cellCount += 1
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+// Prints "6 mansion scenes; 2 cell scenes"
 
-print(dogString.utf8.count)
 
-print(dogString.utf8.count)
 
-print(dogString.unicodeScalars.count)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////Strings Advanced Level
+//
+////Mutliline with " inside without using escaping characters
+//let multiLine = """
+//"hello"
+//"""
+//print(multiLine)
+//
+//
+//let exclamationMark: Character = "!"
+//let catCharacters: [Character] = ["C", "a", "t", "!", "🐱"]
+//
+//var welcome = "Welcome"
+//
+//welcome.append(exclamationMark)
+//
+////You can use extended string delimiters to create strings containing characters that would otherwise be treated as a string interpolation. For example:
+//print(#"Write an interpolated string in Swift using \(multiplier)."#)
+//// Prints "Write an interpolated string in Swift using \(multiplier)."
+//
+//
+//
+////To use string interpolation inside a string that uses extended delimiters, match the number of number signs after the backslash to the number of number signs at the beginning and end of the string. For example:
+//
+//print(#"6 times 7 is \#(6 * 7)."#)
+//// Prints "6 times 7 is 42."
+//
+//let eAcute: Character = "\u{E9}"                         // é
+//let combinedEAcute: Character = "\u{65}\u{301}"          // e followed by
+//// eAcute is é, combinedEAcute is é
+//
+//
+//
+////Extended grapheme clusters enable scalars for enclosing marks (such as COMBINING ENCLOSING CIRCLE, or U+20DD) to enclose other Unicode scalars as part of a single Character value:
+//
+//
+//let copyRight: Character = "\u{63}\u{20DD}"
+//print(copyRight)
+////prints c⃝
+//
+////Unicode scalars for regional indicator symbols can be combined in pairs to make a single Character value, such as this combination of REGIONAL INDICATOR SYMBOL LETTER U (U+1F1FA) and REGIONAL INDICATOR SYMBOL LETTER S (U+1F1F8):
+////https://en.wikipedia.org/wiki/Regional_indicator_symbol
+//
+//let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
+//// regionalIndicatorForUS is 🇺🇸
+//
+//let regionalIndicatorForIndia: Character = "\u{1F1EE}\u{1F1F3}"
+//// regionalIndicatorForIndia is 🇮🇳
+//
+//let stringVal:String = "\u{1F1EE}\u{1F1F3}"
+//print(stringVal.count)
+//
+//let greeting = "Guten Tag!"
+//print(greeting[greeting.index(before: greeting.endIndex)])
+//
+////Uncomment to see the error
+////print(greeting[greeting.endIndex])
+//
+//print(greeting.startIndex) //startIndex will be 1, Not 0
+//
+////To access an index farther away from the given index, you can use the index(_:offsetBy:)
+//let index = greeting.index(greeting.startIndex, offsetBy: 7)
+//print(greeting[index])
+//// a
+//
+////Use the indices property to access all of the indices of individual characters in a string.
+//
+//for index in greeting.indices {
+//    print("\(greeting[index]) ", terminator: "")
+//}
+//// Prints "G u t e n   T a g ! "
+//
+//
+//
+//var welcomeMessage = "hello"
+//welcomeMessage.insert("!", at: welcomeMessage.endIndex)
+//// welcome now equals "hello!"
+//
+//welcomeMessage.insert(contentsOf: " there", at: welcomeMessage.index(before: welcomeMessage.endIndex))
+//// welcome now equals "hello there!"
+//
+//welcomeMessage.remove(at: welcomeMessage.index(before: welcomeMessage.endIndex))
+//// welcome now equals "hello there"
+//
+//let range = welcomeMessage.index(welcomeMessage.endIndex, offsetBy: -6)..<welcomeMessage.endIndex
+//welcomeMessage.removeSubrange(range)
+//// welcome now equals "hello"
+//
+//
+//let greetingMessage = "Hello, world!"
+//let indexxx = greetingMessage.firstIndex(of: ",") ?? greetingMessage.endIndex
+//let beginning = greetingMessage[..<indexxx]
+//// beginning is "Hello"
+//
+//// Convert the result to a String for long-term storage.
+//let newString = String(beginning)
+////Like strings, each substring has a region of memory where the characters that make up the substring are stored. The difference between strings and substrings is that, as a performance optimization, a substring can reuse part of the memory that’s used to store the original string, or part of the memory that’s used to store another substring.
+//
+//
+//
+//// "Voulez-vous un café?" using LATIN SMALL LETTER E WITH ACUTE
+//let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
+//
+//// "Voulez-vous un café?" using LATIN SMALL LETTER E and COMBINING ACUTE ACCENT
+//let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
+//
+//if eAcuteQuestion == combinedEAcuteQuestion {
+//    print("These two strings are considered equal")
+//}
+//// Prints "These two strings are considered equal"
+//
+//let dogString = "Dog‼🐶" // the one used here is not the normal exclamation mark <!> it is <‼>
+//for codeUnit in dogString.utf8 {
+//    print("\(codeUnit) ", terminator: "")
+//}
+//print("")
+//// Prints "68 111 103 226 128 188 240 159 144 182 "
+//
+//for codeUnit in dogString.utf16 {
+//    print("\(codeUnit) ", terminator: "")
+//}
+//print("")
+//// Prints "68 111 103 8252 55357 56374 "
+//
+//
+//for scalar in dogString.unicodeScalars {
+//    print("\(scalar.value) ", terminator: "")
+//}
+//print("")
+//// Prints "68 111 103 8252 128054 "
+//
+//for scalar in dogString.unicodeScalars {
+//    print("\(scalar) ")
+//}
+//
+//for scalar in dogString.unicodeScalars {
+//    if scalar.properties.isEmoji {
+//        print("\(scalar) is an emoji")
+//    }
+//    else {
+//        print("\(scalar) is not an emoji")
+//    }
+//}
+//
+//
+//print(dogString.count)
+//
+//print(dogString.utf8.count)
+//
+//print(dogString.utf8.count)
+//
+//print(dogString.unicodeScalars.count)
+//
 
 //Source form https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html
--- 3.CollectionTypesAdvanced/CollectionTypes.playground/Contents.swift
+++ 3.CollectionTypesAdvanced/CollectionTypes.playground/Contents.swift
@@ -1,9 +1,47 @@
-//Collection Types
+//Collection Types - Array, Set, Dictionary
 
-//Array
+//Mutability of Collections - let and var
+
+
+
+//1.Array
+//An array stores values of the same type in an ordered list. The same value can appear in an array multiple times at different positions.
+
+
+//Creating an Empty Array
+var someInts: [Int] = []
+
+someInts.append(3)
+// someInts now contains 1 value of type Int
+
+//print(someInts)
+
+someInts = []
+// someInts is now an empty array, but is still of type [Int]
+
+
+
+
+
+
+
+
+
+//Creating an Array with a Default Value
 
 var threeDoubles = Array(repeating: 0.0, count: 3)
 // threeDoubles is of type [Double], and equals [0.0, 0.0, 0.0]
+
+
+
+
+
+
+
+
+
+
+//Creating an Array by Adding Two Arrays Together
 
 var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
 // anotherThreeDoubles is of type [Double], and equals [2.5, 2.5, 2.5]
@@ -11,9 +49,21 @@ var anotherThreeDoubles = Array(repeating: 2.5, count: 3)
 var sixDoubles = threeDoubles + anotherThreeDoubles
 // sixDoubles is inferred as [Double], and equals [0.0, 0.0, 0.0, 2.5, 2.5, 2.5]
 
+
+
+
+
+
+
+
+
+
+
+//Creating an Array with an Array Literal
+
 var shoppingList: [String] = ["Eggs", "Milk"]
 
-shoppingList.isEmpty
+shoppingList.isEmpty //false
 
 shoppingList.append("Flour")
 
@@ -21,9 +71,18 @@ shoppingList += ["Baking Powder"]
 shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
 shoppingList
 
+//Retrieve a value from the array by using subscript syntax
+var firstItem = shoppingList[0]
+// firstItem is equal to "Eggs"
+
+
+//You can use subscript syntax to change an existing value at a given index
+shoppingList[0] = "Six eggs"
+// the first item in the list is now equal to "Six eggs" rather than "Eggs"
+
 //uncomment the below to see the runtime error
 //shoppingList[shoppingList.count] = "Salt"
-
+shoppingList
 shoppingList[4...6] = ["Bananas", "Apples"]
 shoppingList
 
@@ -32,46 +91,103 @@ shoppingList
 
 let mapleSyrup = shoppingList.remove(at: 0)
 let apples = shoppingList.removeLast()
+shoppingList
 
-for (index, value) in shoppingList.enumerated() {
-    print("Item \(index + 1): \(value)")
+
+//Iterating Over an Array
+for item in shoppingList {
+//    print(item)
 }
 
 
-//Sets
+for (index, value) in shoppingList.enumerated() {
+//    print("Item \(index + 1): \(value)")
+}
 
 
-var letters = Set<Character>()
-print("letters is of type Set<Character> with \(letters.count) items.")
 
-letters.insert("a")
-// letters now contains 1 value of type Character
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2.Sets
+//A set stores distinct values of the same type in a collection with no defined ordering. You can use a set instead of an array when the order of items isn’t important, or when you need to ensure that an item only appears once.
+
+//Creating and Initializing an Empty Set
+
+var letters = Set<String>()
+//print("letters is of type Set<String> with \(letters.count) items.")
+
+letters.insert("Lal")
+// letters now contains 1 value of type String
 letters = []
-// letters is now an empty set, but is still of type Set<Character>
+// letters is now an empty set, but is still of type Set<String>
 
+
+
+
+//Creating a Set with an Array Literal
 var favoriteGenress: Set<String> = ["Rock", "Classical", "Hip hop", "Hip hop"]
-print(favoriteGenress)
+//print(favoriteGenress)
 
 var favoriteGenres: Set = ["Rock", "Classical", "Hip hop"]
 
+
+
+
+
+//Accessing and Modifying a Set
 favoriteGenres.insert("Jazz")
 
 if let removedGenre = favoriteGenres.remove("Rock") {
-    print("\(removedGenre)? I'm over it.")
+//    print("\(removedGenre)? I'm over it.")
 } else {
-    print("I never much cared for that.")
+//    print("I never much cared for that.")
 }
 // Prints "Rock? I'm over it."
 
-print(favoriteGenres.contains("Funk"))
+
+
+
+//print(favoriteGenres.contains("Funk"))
+
+
+
+//Iterating Over a Set
 
 for genre in favoriteGenres {
-    print("\(genre)")
+//    print("\(genre)")
 }
 
 for genre in favoriteGenres.sorted() {
-    print("\(genre)")
+//    print("\(genre)")
 }
+
+
+
+
+//Performing Set Operations
 
 let oddDigits: Set = [1, 3, 5, 7, 9]
 let evenDigits: Set = [0, 2, 4, 6, 8]
@@ -85,6 +201,11 @@ oddDigits.subtracting(singleDigitPrimeNumbers).sorted()
 // [1, 9]
 oddDigits.symmetricDifference(singleDigitPrimeNumbers).sorted()
 // [1, 2, 9]
+
+
+
+
+
 
 //Use the “is equal” operator (==) to determine whether two sets contain all of the same values.
 //Use the isSubset(of:) method to determine whether all of the values of a set are contained in the specified set.
@@ -110,22 +231,62 @@ farmAnimals.isStrictSuperset(of: houseAnimals)
 houseAnimals.isStrictSubset(of: farmAnimals)
 //true
 
-setA == setB
-setA.isStrictSubset(of: setB)
-setB.isStrictSuperset(of: setB)
+setA == setB // true
+setA.isStrictSubset(of: setB) // false
+setB.isStrictSuperset(of: setB) // false
 
 
 
-//Dictionary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//3.Dictionary
 var namesOfIntegers: [Int: String] = [:]
+//var serverResponse: [String: Any] = [:]
 namesOfIntegers[16] = "sixteen"
+
+namesOfIntegers
 // namesOfIntegers now contains 1 key-value pair
 namesOfIntegers = [:]
 // namesOfIntegers is once again an empty dictionary of type [Int: String]
 
-var airports = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+
+
+
+
+
+
+var airports = ["YYZ": "Toronto Pearson", "DUB": "Dublin"] // [String: String]
 airports["LHR"] = "London"
 airports["LHR"] = "London Heathrow"
+airports
+
+
 
 //As an alternative to subscripting, use a dictionary’s updateValue(_:forKey:) method to set or update the value for a particular key.
 if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
@@ -133,12 +294,19 @@ if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
 }
 // Prints "The old value for DUB was Dublin."
 
+airports
+
+
 if let airportName = airports["DUB"] {
     print("The name of the airport is \(airportName).")
 } else {
     print("That airport isn't in the airports dictionary.")
 }
 // Prints "The name of the airport is Dublin Airport."
+
+
+
+
 
 //You can use subscript syntax to remove a key-value pair from a dictionary by assigning a value of nil for that key:
 
@@ -153,6 +321,13 @@ if let removedValue = airports.removeValue(forKey: "DUB") {
     print("The airports dictionary doesn't contain a value for DUB.")
 }
 // Prints "The removed airport's name is Dublin Airport."
+
+airports
+
+
+
+
+
 
 for (airportCode, airportName) in airports {
     print("\(airportCode): \(airportName)")
@@ -175,6 +350,9 @@ for airportCode in airports.keys.sorted() {
 for airportName in airports.values.sorted() {
     print("Airport name: \(airportName)")
 }
+
+
+
 
 //If you need to use a dictionary’s keys or values with an API that takes an Array instance, initialize a new array with the keys or values property:
 
