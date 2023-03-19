a8e2c9074313a6b3e857a2bb40f4fc61e5479089
Lal Castro <lal.castro@icloud.com>
2023-03-19 09:07:40 +0530

    Functions

--- 5.Functions/Functions.playground/Contents.swift
+++ 5.Functions/Functions.playground/Contents.swift
@@ -1,8 +1,160 @@
+import Foundation
 
-//Functions advanced
+//let dateFormatter = DateFormatter()
+//dateFormatter.dateFormat = "dd-MMM-yyyy"
+//
+
+//let milliseconds: Double = 1616176200000 // example milliseconds
+//let date = Date(timeIntervalSince1970: milliseconds/1000)
+//let dateString = dateFormatter.string(from: date)
+//print(dateString)
+//
+//let milliseconds2: Double = 4561786200000 // example milliseconds
+//let date2 = Date(timeIntervalSince1970: milliseconds2/1000)
+//let dateString2 = dateFormatter.string(from: date2)
+//print(dateString2)
 
 
-//The return value of a function can be ignored when it’s called:
+//
+//func convertMilliSecondsToDate(milliseconds: Double) -> String {
+//    let date = Date(timeIntervalSince1970: milliseconds/1000)
+//    let dateFormatter = DateFormatter()
+//    dateFormatter.dateFormat = "dd-MMM-yyyy"
+//    let dateString = dateFormatter.string(from: date)
+//    return dateString
+//}
+//
+//print(convertMilliSecondsToDate(milliseconds: 1616176200000))
+//print(convertMilliSecondsToDate(milliseconds: 4561786200000))
+//print(convertMilliSecondsToDate(milliseconds: 2361786200000))
+
+
+
+
+
+
+
+
+
+
+//Defining and Calling Functions
+
+func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+}
+
+print(greet(person: "Anna"))
+// Prints "Hello, Anna!"
+print(greet(person: "Brian"))
+// Prints "Hello, Brian!"
+
+
+
+func greetAgain(person: String) -> String {
+    return "Hello again, " + person + "!"
+}
+print(greetAgain(person: "Anna"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Functions Without Parameters
+
+func sayHelloWorld() -> String {
+    return "hello everyone"
+}
+print(sayHelloWorld())
+// Prints "hello everyone"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Functions With Multiple Parameters
+
+func greet(person: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return greetAgain(person: person)
+    } else {
+        return greet(person: person)
+    }
+}
+print(greet(person: "Tim", alreadyGreeted: true))
+// Prints "Hello again, Tim!"
+
+
+
+
+
+
+
+
+
+
+
+
+//Functions Without Return Values
+
+
+func greet1(person: String) {
+    print("Hello, \(person)!")
+}
+greet1(person: "Dave")
+// Prints "Hello, Dave!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Ignoring return value
+
+
 func printAndCount(string: String) -> Int {
     print(string)
     return string.count
@@ -15,23 +167,56 @@ printAndCount(string: "hello, world")
 printWithoutCounting(string: "hello, world")
 // prints "hello, world" but doesn't return a value
 
-//func minMax(array: [Int]) -> (min: Int, max: Int) {
-//    var currentMin = array[0]
-//    var currentMax = array[0]
-//    for value in array[1..<array.count] {
-//        if value < currentMin {
-//            currentMin = value
-//        } else if value > currentMax {
-//            currentMax = value
-//        }
-//    }
-//    return (currentMin, currentMax)
-//}
-if let bounds = minMax(array: [8, -6, 2, 109, 3, 71]) {
-    print("min is \(bounds.min) and max is \(bounds.max)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Functions with Multiple Return Values
+
+
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
 }
 
-func minMax(array: [Int]) -> (min: Int, max: Int)? {
+let bounds = minMax(array: [8, -6, 2, 109, 3, 71])
+print("min is \(bounds.min) and max is \(bounds.max)")
+
+
+
+
+
+
+
+
+
+
+
+
+//Optional Return Types
+func minMax1(array: [Int]) -> (min: Int, max: Int)? {
     if array.isEmpty { return nil }
     var currentMin = array[0]
     var currentMax = array[0]
@@ -44,21 +229,99 @@ func minMax(array: [Int]) -> (min: Int, max: Int)? {
     }
     return (currentMin, currentMax)
 }
+
+if let bounds = minMax1(array: [8, -6, 2, 109, 3, 71]) {
+    print("min is \(bounds.min) and max is \(bounds.max)")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Functions With an Implicit Return
+
 func greeting(for person: String) -> String {
     "Hello, " + person + "!"
 }
 print(greeting(for: "Dave"))
 // Prints "Hello, Dave!"
 
+func anotherGreeting(for person: String) -> String {
+    return "Hello, " + person + "!"
+}
+print(anotherGreeting(for: "Dave"))
+// Prints "Hello, Dave!"
 
-//The argument label is used when calling the function
-//By default, parameters use their parameter name as their argument label if argument label is not specified.
+
+
+
+
+
+
+
+
+
+//Function Argument Labels and Parameter Names
+
+func someFunction(firstParameterName: Int, secondParameterName: Int) {
+    // In the function body, firstParameterName and secondParameterName
+    // refer to the argument values for the first and second parameters.
+}
+someFunction(firstParameterName: 1, secondParameterName: 2)
+
+
+
+
+
+
+
+
+func someFunction(argumentLabel parameterName: Int) {
+    // In the function body, parameterName refers to the argument value
+    // for that parameter.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 func greet(person: String, from hometown: String) -> String {
     return "Hello \(person)!  Glad you could visit from \(hometown)."
 }
 print(greet(person: "Bill", from: "Cupertino"))
 // Prints "Hello Bill!  Glad you could visit from Cupertino."
+
+
+
+
+
+
+
+
+
 
 //Omitting Argument Labels
 
@@ -68,15 +331,46 @@ func someFunction(_ firstParameterName: Int, secondParameterName: Int) {
 }
 someFunction(1, secondParameterName: 2)
 
-//Place parameters that don’t have default values at the beginning of a function’s parameter list
-func someFunctionn(parameterWithoutDefault: Int, parameterWithDefault: Int = 12) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Default Parameter Values
+
+func someFunction(parameterWithoutDefault: Int, parameterWithDefault: Int = 12) {
     // If you omit the second argument when calling this function, then
     // the value of parameterWithDefault is 12 inside the function body.
 }
-someFunctionn(parameterWithoutDefault: 3, parameterWithDefault: 6) // parameterWithDefault is 6
-someFunctionn(parameterWithoutDefault: 4) // parameterWithDefault is 12
+someFunction(parameterWithoutDefault: 3, parameterWithDefault: 6) // parameterWithDefault is 6
+someFunction(parameterWithoutDefault: 4) // parameterWithDefault is 12
+
+
+
+
+
+
+
+
+
+
+
 
 //Variadic Parameters
+
 func arithmeticMean(_ numbers: Double...) -> Double {
     var total: Double = 0
     for number in numbers {
@@ -89,17 +383,22 @@ arithmeticMean(1, 2, 3, 4, 5)
 arithmeticMean(3, 8.25, 18.75)
 // returns 10.0, which is the arithmetic mean of these three numbers
 
-//The first parameter that comes after a variadic parameter must have an argument label.
+
+
+
+
+
+
+
 
 
 //In-Out Parameters
-//You can only pass a variable as the argument for an in-out parameter. You can’t pass a constant or a literal value as the argument, because constants and literals can’t be modified.
-//In-out parameters can’t have default values, and variadic parameters can’t be marked as inout.
 func swapTwoInts(_ a: inout Int, _ b: inout Int) {
     let temporaryA = a
     a = b
     b = temporaryA
 }
+
 
 var someInt = 3
 var anotherInt = 107
@@ -108,29 +407,70 @@ print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")
 // Prints "someInt is now 107, and anotherInt is now 3"
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+//Function Types
 func addTwoInts(_ a: Int, _ b: Int) -> Int {
     return a + b
 }
 func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
     return a * b
 }
+// (Int, Int) -> Int
+
+func printHelloWorld() {
+    print("hello, world")
+}
+// () -> Void
+
 var mathFunction: (Int, Int) -> Int = addTwoInts
 
 print("Result: \(mathFunction(2, 3))")
+// Prints "Result: 5"
 
 mathFunction = multiplyTwoInts
 print("Result: \(mathFunction(2, 3))")
 // Prints "Result: 6"
 
+
 let anotherMathFunction = addTwoInts
 // anotherMathFunction is inferred to be of type (Int, Int) -> Int
 
+
+
+
+
+
+
+
 //Function Types as Parameter Types
+
 func printMathResult(_ mathFunction: (Int, Int) -> Int, _ a: Int, _ b: Int) {
     print("Result: \(mathFunction(a, b))")
 }
 printMathResult(addTwoInts, 3, 5)
 // Prints "Result: 8"
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Function Types as Return Types
@@ -145,8 +485,9 @@ func chooseStepFunction(backward: Bool) -> (Int) -> Int {
     return backward ? stepBackward : stepForward
 }
 
-var currentValue = 5
+var currentValue = 3
 let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+// moveNearerToZero now refers to the stepBackward() function
 
 print("Counting to zero:")
 // Counting to zero:
@@ -161,18 +502,20 @@ print("zero!")
 // zero!
 
 
-//Nested Function
-func chooseStepFunctionn(backward: Bool) -> (Int) -> Int {
+
+//Nested Functions
+
+func chooseStepFunction1(backward: Bool) -> (Int) -> Int {
     func stepForward(input: Int) -> Int { return input + 1 }
     func stepBackward(input: Int) -> Int { return input - 1 }
     return backward ? stepBackward : stepForward
 }
-var currentValuee = -4
-let moveNearerToZeroo = chooseStepFunction(backward: currentValuee > 0)
+var currentValue1 = -4
+let moveNearerToZero1 = chooseStepFunction1(backward: currentValue1 > 0)
 // moveNearerToZero now refers to the nested stepForward() function
-while currentValuee != 0 {
-    print("\(currentValuee)... ")
-    currentValuee = moveNearerToZeroo(currentValuee)
+while currentValue1 != 0 {
+    print("\(currentValue1)... ")
+    currentValue1 = moveNearerToZero1(currentValue1)
 }
 print("zero!")
 // -4...
